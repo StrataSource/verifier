a01ae09fa9db1e0ccfe43326ecfa563a482da3f1
craftablescience <me@craftablescience.info>
2024-07-19 21:25:52 -0400

    feat: remove fmt from log functions

--- src/log.cpp
+++ src/log.cpp
@@ -1,41 +1,39 @@
 #include "log.hpp"
-#include <iostream>
+
+#include <cstdio>
 
 bool g_bUIReportMode = false;
 
-void Log_Message( LogSeverity severity, const std::string_view message )
+void Log_Message( LogSeverity severity, std::string_view message )
 {
 	// Don't log in report only mode!
-	const char* prefixes[] = {
+	static constexpr std::string_view prefixes[] = {
 		"Info",
 		"Warn",
 		"Error",
 	};
-	const char* prefix = prefixes[(int)severity];
+	const auto prefix{ prefixes[ static_cast<int>( severity ) ] };
 
 	if ( g_bUIReportMode )
 	{
 		// Print for UI to read
-		const auto line{ fmt::format( R"("message","{}","{}",,)", prefix, message ) };
-		std::printf( "%s\n", line.c_str() );
+		std::printf( R"("message","%s","%s",,)" "\n", prefix.data(), message.data() );
 	}
 	else
 	{
 		// Standard print
-		std::cout << prefix << ": " << message << "\n";
+		std::printf("%s: %s\n", prefix.data(), message.data());
 	}
 }
 
-void Log_Report( const std::string_view file, const std::string_view message, const std::string_view got, const std::string_view expected )
+void Log_Report( std::string_view file, std::string_view message, std::string_view got, std::string_view expected )
 {
 	if ( g_bUIReportMode )
 	{
-		const  auto line{ fmt::format( R"("report","{}","{}","{}","{}")", file, message, got, expected ) };
-		std::printf( "%s\n", line.c_str() );
+		std::printf( R"("report","%s","%s","%s","%s")" "\n", file.data(), message.data(), got.data(), expected.data() );
 	}
 	else
 	{
-		const auto line{ fmt::format( "In file `{}`: {}", file, message ) };
-		std::fprintf( stderr, "%s\n", line.c_str() );
+		std::fprintf( stderr, "In file `%s`: %s\n", file.data(), message.data() );
 	}
 }
