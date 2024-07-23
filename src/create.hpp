122af7951d998c0abea7244b021d6f88167dbc36
tsa96 <tom@trumpetdust.org>
2024-07-23 03:23:26 +0100

    refactor: split --include and --exclude into file and archive variants

--- src/create.hpp
+++ src/create.hpp
@@ -6,4 +6,6 @@
 #include <string_view>
 #include <vector>
 
-auto createFromRoot( std::string_view root, std::string_view indexLocation, bool skipArchives, const std::vector<std::string>& excluded, const std::vector<std::string>& included ) -> int;
+auto createFromRoot( std::string_view root_, std::string_view indexLocation, bool skipArchives,
+					 const std::vector<std::string>& fileExcludes, const std::vector<std::string>& fileIncludes,
+					 const std::vector<std::string>& archiveExcludes, const std::vector<std::string>& archiveIncludes) -> int;
